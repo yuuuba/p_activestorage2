d9dbaf6bea394f387909c58da7ef840e683c35bd
yuuuba <nsabu45@gmail.com>
2023-06-09 22:28:01 +0900

    ログアウトできない問題解決

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  get "users/:id", to: "users#show"
+  get "mypage", to: "users#mypage"
 end
